b03c6943f7593d2b5c37c0b93728db2ed47a1495
mzholudeva <mzholudeva@yandex.ru>
2024-02-26 10:31:01 +0200

    Версия-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content; 
+const cardTemplate = document.querySelector('#card-template').content;
 
 
 // @todo: DOM узлы
+
 const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function addCard(name, link){
+function createCard(name, link, onDeleteCard){
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   cardImage.setAttribute('src', link);
@@ -15,17 +16,25 @@ function addCard(name, link){
   cardTitle.textContent = name;
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', onDeleteCard);
+  return cardElement;
+}
+
+// @todo: Функция добавления карточки в DOM
+
+function addCard(cardElement){
   cardContainer.append(cardElement);
 }
 
 // @todo: Функция удаления карточки
 
 function onDeleteCard(event){
-  const cardElement = event.target.closest('.card'); 
+  const cardElement = event.target.closest('.card');
   cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
+
 initialCards.forEach(function (card){
-  addCard(card.name, card.link);
+  const cardElement = createCard(card.name, card.link, onDeleteCard);
+  addCard(cardElement);
 })
